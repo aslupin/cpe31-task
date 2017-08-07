4ffbc322afa7ef319da7420a8eba590285af3df1
poonsht <poon_arsene_lupin@hotmail.com>
2017-08-07 21:28:22 +0700

    firstcommit

---
+++ week1/decrypt.py
@@ -0,0 +1,8 @@
+##################################################
+## Problem: one_word_per_line.py
+## Std: std09
+## Name: Poon Shotateerawasu
+## StudentId: 6010500109
+##################################################
+txt = input()
+print(str(txt.replace(" ","\n")))
