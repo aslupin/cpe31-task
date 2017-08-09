a446b767d267a177caf668614af007efb0a542c2
poonsht <poon_arsene_lupin@hotmail.com>
2017-08-09 15:06:55 +0700

    triangle-valid-homework

--- week2/stack_condition.py
+++ week2/stack_condition.py
@@ -1,1 +1,24 @@
-:
+def get_top(topostack,stack,stackii,stackiii):
+	if(stack.)
+	topostack.append(stack.top())
+	topostack.sort()
+def pocess_swapval(stack,stackii,stackiii):	
+	print(" ") 
+
+# var
+topostack = []
+# input
+num = int(input())
+#stack = stackii = stackiii = list()
+stack = list()
+stackii = list()
+stackiii = list()
+
+# get data
+data = num
+for i in range(num): stack.append(num-i)
+
+# travel stack
+print("stack : "+str(stack))
+print("stack ii : "+str(stackii))
+print("stack iii : "+str(stackiii))
---
+++ week2/triangle_valid.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if(a+b > c and a+c > b and b+c > a) : print('triangle is valid')
+else : print('triangle is not vai')
