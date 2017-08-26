93371de6ce9365df9680bec617f3e34e797b6cbe
poonsht <poon_arsene_lupin@hotmail.com>
2017-08-26 18:07:56 +0700

    fix : hilo calculater and complete : crossriver

--- week4/cross_river.py
+++ week4/cross_river.py
@@ -1,25 +1,27 @@
-count_ppl=0;count_wet=0;ans=0
+count_wet=0;ans=0;count_ppl=0
 ppl = list();wet = list()
 ppl.append(int(input())) # at index 0 = max ppl
-wet.append(int(input())) # at index 0 = max weigth
+wet.append(float(input())) # at index 0 = max weigth
 while(True):
     n = int(input())
     if(n == -1):break
-    w = int(input())
+    w = float(input())
     if(w == -1):break
-    ppl.append(n)
-    wet.append(w)
-# loop in each group-ppl
+    if(w <= wet[0]):
+        ppl.append(n)
+        wet.append(w)
+print(wet,ppl)
 for i in range(1,len(ppl)):
+    # print(i)
     while(True):
+        count_wet += wet[i]
         ppl[i] -= 1
         count_ppl += 1
-        count_wet += wet[i]
-        if(count_wet + wet[i] > wet[0] or count_ppl == ppl[0]):
-            print("{} {} {}".format(count_ppl,count_wet,ppl[i]))
+        if(count_wet + wet[i] > wet[0] or count_ppl == ppl[0] or (ppl[i] == 0 and i == len(ppl)-1)):
+            # print("{} {} {} {}".format(i,ans,count_wet,ppl[i]))
             ans += 1
-            count_ppl = 0
             count_wet = 0
+            count_ppl = 0
         if(ppl[i] == 0):break
 print(ans)
 
