d6ab93ae6b8827237aa9607d420c023f74b5e520
poonsht <poon_arsene_lupin@hotmail.com>
2017-10-16 03:01:11 +0700

    fix lint : del debug point

--- week8/special_mode.py
+++ week8/special_mode.py
@@ -1,19 +1,15 @@
 def ch_sp_mod(key,f,ans=[],save=None):
     if(not key):return False
     ans.append(key[f.index(max(f))])
-    # print('first',key,f)
     key.pop(f.index(max(f)));save = f.pop(f.index(max(f)))
-    # print('first',key,f)
     if(len(key) > 0):
         while(max(f) == save):
             if(not (key[f.index(max(f))] in ans)):ans.append(key[f.index(max(f))])
             key.pop(f.index(max(f)));f.pop(f.index(max(f)))
             if(not f or not key):break
-    # print(ans)
     if(len(ans) <= 2):
         if(len(ans) == 1):print(ans[0])
         else:print(ans[0],ans[1])
-        # for i in ans:print(i,end=" ")
         return True
     else:return False
 
@@ -30,7 +26,6 @@ while(txt != 'end'):
             key.append(txt)
             f.append(1)
             i+=1
-    # print(key,f)
     txt = input()
 if(ch_sp_mod(key,f)):
     pass
